66c10a06fe5f5232cbb0ba692da1a3922f6d8ad2
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2021-06-07 22:26:11 +0200

    mise en place des champs info nom et prénom, valides et non vides

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -21,6 +21,12 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+
+
+
+
+
 //fermeture du formulaire
 //close modal event
  btnClose.addEventListener("click", closeModal);
@@ -33,7 +39,7 @@ function closeModal() {
 
 
 
-// pour repondre au champ non vide : ajout de l'attribut required au formulaire avec une boucle for...of
+// pour repondre au champs non vide : ajout de l'attribut required au formulaire avec une boucle for...of
 
 const infoFirstName = document.getElementById("first");
 const infoLastName = document.getElementById("last");
@@ -46,6 +52,22 @@ let infoForm = [infoFirstName,infoLastName, infoEmail , infoBirthdate, infoNumbe
 for ( let infoRequiered of infoForm){
   console.log(infoRequiered.setAttribute("required",""));
 }
+
+// 
+
+//champ info prénom : valide
+infoFirstName.setAttribute("pattern", "[A-Z]{1}[a-z -]{2,30}[a-z]");
+infoFirstName.setAttribute("placeholder", "Jean-paul");
+
+// champ info nom: valide
+
+infoLastName.setAttribute("minlength", "2");
+infoLastName.setAttribute("pattern", "[A-Z -]{2,30}[A-Z]");
+infoLastName.setAttribute("placeholder", "DUPOND-MARTIN");
+
+//pattern email
+
+
 
 
 
