1c1e40b39fc67af7eb2de6548d96260e23f9f778
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2021-06-13 00:09:37 +0200

    optimisation des  messages d'erreur du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -22,6 +22,8 @@ const minlength = document.getElementsByTagName("minlength");
 const gameonMatch = document.getElementById("quantity");
 const locationCity = document.querySelector(".city");
 const termsOfUse = document.getElementById("checkbox1");
+const maxlength = document.getElementsByTagName("maxlength");
+const cityChecked = document.getElementsByTagName("checked");
 
 //affichage du formulaire
 // launch modal event
@@ -45,98 +47,154 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-/***********************************************************************validation du formulaire*************************************************************/
+/***********************************************************************fonctions validation du formulaire************************************************************/
+let regexName = /^[a-z ,.'-]+$/i; 
 
 
-
-
-
-//rattrachement au bouton d'envoie
-submitForm.addEventListener("click", validate);
-
-//fonction validation des inputs
-function validate(event){
-  event.preventDefault(); //annule l'action par défaut du requiered
-  //validation prénom
-  infoFirstName.addEventListener("blur", function(event){
+function validFirstName(){
+  
+   //validation prénom
+   infoFirstName.addEventListener("keyup", function(event){
+    event.preventDefault();
+    
+   
     if(infoFirstName.validity.valueMissing){
       infoFirstName.closest(".formData").setAttribute("data-error", "Veuillez remplir le formulaire");
-    }else if (infoFirstName.validity.patternMismatch){
-      infoFirstName.closest(".formData").setAttribute("data-error", "attention : lettres MINUSCULES, pas de chiffre ni de caractères speciaux" );
-    }else{
-      infoFirstName.closest(".formData").setAttribute("data-error","");
+      return false;
+    }else if (regexName.test(infoFirstName.value)==false){
+      infoFirstName.closest(".formData").setAttribute("data-error", "pas de nombre, caractères autorisés: (. - ')");
+      return false;
+    }else if (infoFirstName.value.length < 2){
+      infoFirstName.closest(".formData").setAttribute("data-error", "Veuillez entrer entre 2 et 30 caractères pour valider ce champ " );
+      return false;
+      
+    }
+    else{
+      infoFirstName.closest(".formData").setAttribute("data-error", "" );
       return true;
+      
     }
-  });
-  //validation nom
-  infoLastName.addEventListener("blur", function(event){
-    if(infoLastName.validity.valueMissing){
-      infoLastName.closest(".formData").setAttribute("data-error", "Veuillez remplir ce champ");
-    }else if (infoLastName.value < minlength){
-      infoLastName.closest(".formData").setAttribute("data-error", "Veuillez entrer 2 caractères minimum" );
-    }else if (infoLastName.validity.patternMismatch){
-      infoLastName.closest(".formData").setAttribute("data-error", "attention: lettres MAJUSCULES pas de chiffre ni de caractères speciaux" );
-    }else{
-      infoLastName.closest(".formData").setAttribute("data-error","");
-    }
-  });
-  //validation email
-  infoEmail.addEventListener("blur", function(event){
-    if(infoEmail.validity.valueMissing){
-      infoEmail.closest(".formData").setAttribute("data-error", "Veuillez remplir ce champ");
-    }else if (infoEmail.validity.typeMismatch){
-      infoEmail.closest(".formData").setAttribute("data-error", "Veuillez entrer une adresse email valide (par exemple:  monemail@gmail.com)");
-    }else{
-      infoEmail.closest(".formData").setAttribute("data-error","");
-    }
-  });
-  // validation date de naissance
-infoBirthdate.addEventListener("blur", function(event){
-  if(infoBirthdate.validity.valueMissing){
-    infoBirthdate.closest(".formData").setAttribute("data-error", "Veuillez remplir ce champ");
-  }else{
-    infoBirthdate.closest(".formData").setAttribute("data-error","");
-  }
-  });
-  //validation experience gameOn
-gameonMatch.addEventListener("blur", function(event){
-  if(gameonMatch.validity.valueMissing){
-    gameonMatch.closest(".formData").setAttribute("data-error", "Veuillez remplir ce champ");
-  }else{
-    gameonMatch.closest(".formData").setAttribute("data-error","");
-  }
-  });
-  //validation villes : ! ne fonctionne pas
-    if(locationCity.validity.typeMismatch){
-      locationCity.closest(".formData").setAttribute("data-error", "Veuillez remplir ce champ");
-    }else{
-      locationCity.closest(".formData").setAttribute("data-error","");
-    };
-  //validation condition d'utilisation
-  termsOfUse.addEventListener("change", function(event){
-    if(this.checked == false){
-      termsOfUse.closest(".formData").setAttribute("data-error", "Veuillez accepter les conditions d'utilisation");
-    }else{
-      infoBirthdate.closest(".formData").setAttribute("data-error","");
-    }
-    }); 
-
-
+})
 }
 
-
-/*function validate(){
-  infoFirstName.addEventListener("input", function(event){
+function validLastName(){
+  //validation nom
+  infoLastName.addEventListener("keyup", function(event){
     event.preventDefault();
-    if (infoFirstName.validity.valueMissing){
-      document.getElementById("TextInvalid").innerHTML="Veuillez entrer votre prénom (exemple:Jean-paul)";
+    if(infoLastName.validity.valueMissing){
+      infoLastName.closest(".formData").setAttribute("data-error", "Veuillez remplir ce champ");
       return false;
-    }else if(infoFirstName.value !== "[A-Z]{1}[a-z -]{2,30}[a-z]"){
-      document.getElementById("TextInvalid").innerHTML = "Votre prénom doit commencer par une majuscule et peut comporter un (-) et non des chiffres ";
+    }else if (regexName.test(infoLastName.value)==false){
+      infoLastName.closest(".formData").setAttribute("data-error", "pas de nombre, caractères autorisés: (. - ')" );
       return false;
-  }else{
-    document.getElementById("TextInvalid").innerHTML = "";
-  }
-  });
+    }else if (infoLastName.value.length < 2){
+      infoLastName.closest(".formData").setAttribute("data-error", "Veuillez entrer entre 2 et 30 caractères pour valider ce champ " );
+      return false;
+ 
+    }else{
+      infoLastName.closest(".formData").setAttribute("data-error","");
+      return true;
+    }
+})
+}
 
-}*/
+function validEmail(){
+   //validation email
+   infoEmail.addEventListener("keyup", function(event){
+    if(infoEmail.validity.valueMissing){
+      infoEmail.closest(".formData").setAttribute("data-error", "Veuillez remplir ce champ");
+      return false;
+    }else if (infoEmail.validity.typeMismatch){
+      infoEmail.closest(".formData").setAttribute("data-error", "Veuillez entrer une adresse email valide (par exemple:  monemail@gmail.com)");
+      return false;
+    }else{
+      infoEmail.closest(".formData").setAttribute("data-error","");
+      return true;
+    }
+})
+}
+//validation experience gameOn
+function infoMatchGameon(){
+  gameonMatch.addEventListener("keyup", function(event){
+    event.preventDefault();
+  if(gameonMatch.validity.typeMismatch){
+    gameonMatch.closest(".formData").setAttribute("data-error", "Veuillez remplir ce champ");
+    return false;
+  }else if (gameonMatch.value > 99){
+    gameonMatch.closest(".formData").setAttribute("data-error", "Veuillez indiquer un nombre entre 1 et 99");
+    return false;
+  }
+  else{
+    gameonMatch.closest(".formData").setAttribute("data-error","");
+    return true;
+  }
+});
+}
+
+function validBirthdate(){
+   // validation date de naissance
+   infoBirthdate.addEventListener("blur", function(event){
+    event.preventDefault();
+  if(infoBirthdate.validity.valueMissing){
+    infoBirthdate.closest(".formData").setAttribute("data-error", "Veuillez remplir ce champ");
+    return false;
+  }else{
+    infoBirthdate.closest(".formData").setAttribute("data-error","");
+    return true;
+  }
+})
+}
+
+function validCheckCity(){
+    //validation villes : ! ne fonctionne pas
+    if(locationCity.validity.typeMismatch){
+      locationCity.closest(".formData").setAttribute("data-error", "Veuillez remplir ce champ");
+      return false;
+    }else if(locationCity.checked==false){
+      locationCity.closest(".formData").setAttribute("data-error", "Veuillez choisir une ville");
+      return false; 
+    }else{
+      locationCity.closest(".formData").setAttribute("data-error","");
+     
+      return true;
+    };
+}
+
+function validTermsOfUse(){
+   //validation condition d'utilisation
+   termsOfUse.addEventListener("change", function(event){
+    event.preventDefault();
+      if(this.checked == false){
+        termsOfUse.closest(".formData").setAttribute("data-error", "Veuillez accepter les conditions d'utilisation");
+        return false;
+      }else{
+        infoBirthdate.closest(".formData").setAttribute("data-error","");
+        return true;
+      };  
+  });
+}
+
+/*******************************************************************************************************************************************************/
+
+let listInputValid =
+validFirstName();
+validLastName(),
+validEmail();
+validBirthdate();
+infoMatchGameon();
+validCheckCity();
+validTermsOfUse();
+
+
+//fonction message d'envoie formulaire
+function submitTheForm(){
+  submitForm.addEventListener("click", function (){
+    alert("Merci votre formulaire a été envoyé");
+    
+  })
+}
+
+submitTheForm();
+
+
+
